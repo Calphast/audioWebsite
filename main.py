6879cc24f83526293f7a06e5dd8899395aa48fc5
Calpha <gabrielnsouto100@gmail.com>
2024-12-10 21:32:08 -0500

    Added fastapi support

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.requests import Request
+
+app = FastAPI()
+
+# Serve static files (CSS, JS, Images, etc.)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Set up templates directory
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/about-us", response_class=HTMLResponse)
+async def about_us(request: Request):
+    return templates.TemplateResponse("about-us.html", {"request": request})
