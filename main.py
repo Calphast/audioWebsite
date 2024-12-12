3ede915f47ee6c4faa585f87a1c3844a655d3c88
Calpha <gabrielnsouto100@gmail.com>
2024-12-12 14:49:33 -0500

    Changed colors, added downloads

--- main.py
+++ main.py
@@ -12,10 +12,17 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 # Set up templates directory
 templates = Jinja2Templates(directory="templates")
 
+
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
+
 @app.get("/about-us", response_class=HTMLResponse)
 async def about_us(request: Request):
     return templates.TemplateResponse("about-us.html", {"request": request})
+
+
+@app.get("/downloads", response_class=HTMLResponse)
+async def about_us(request: Request):
+    return templates.TemplateResponse("downloads.html", {"request": request})
